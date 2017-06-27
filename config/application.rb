4e30f3daf62af6eb4bd3a337a12405ffe9a4ab52
dbfdyddyf <dbfdyddyf@192.168.3.44>
2017-06-27 20:16:07 +0800

    origin

--- config/application.rb
+++ config/application.rb
@@ -57,11 +57,10 @@ module Prelaunchr
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.ended = true
+    config.ended = false
   end
 end
